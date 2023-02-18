d49c564d65f6080ee31b87298c2721d62885aea9
giorgibliadze <bliadze1997@gmail.com>
2023-02-18 04:14:36 +0400

    property

--- src/App.tsx
+++ src/App.tsx
@@ -115,28 +115,33 @@ function App() {
           catchAll={<ErrorComponent />}
           resources={[
             {
-              name: "property",
-              list: MuiInferencer,
+              name: "properties",
+              list: AllProperties,
+              show: PropertyDetails,
+              create: CreateProperty,
+              edit: EditProperty,
+
               icon: <VillaOutlined />,
             },
             {
-              name: "agent",
-              list: MuiInferencer,
+              name: "agents",
+              list: Agents,
+              show: AgentProfile,
               icon: <PeopleAltOutlined />,
             },
             {
-              name: "review",
-              list: MuiInferencer,
+              name: "reviews",
+              list: Home,
               icon: <StarOutlineRounded />,
             },
             {
-              name: "message",
-              list: MuiInferencer,
+              name: "messages",
+              list: Home,
               icon: <ChatBubbleOutlined />,
             },
             {
               name: "my-profile",
-              list: MuiInferencer,
+              list: MyProfile,
               options: { label: "My Profile" },
               icon: <AccountCircleOutlined />,
             },
